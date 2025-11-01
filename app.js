5f66e162f816266bf8a1e846353ace1b11799b74
Eren ALTAY <eren60.altay@gmail.com>
2025-11-01 20:10:46 +0300

    Dynamic Photos

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
 const express = require('express');
+// Load environment variables from .env into process.env
+require('dotenv').config();
+
+const connectDB = require('./config/db');
 const ejs = require('ejs');
+const Photo = require('./models/Photo');
 
 const app = express();
+connectDB();
 
 app.set('view engine', 'ejs');
 
 
+
 // Middleware
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Routes
-app.get('/', (req, res) => {
-    res.render('index');
+app.get('/', async (req, res) => {
+    const photos = await Photo.find({});
+    res.render('index', {
+        photos
+    });
 });
 app.get('/about', (req, res) => {
     res.render('about');
@@ -20,7 +32,12 @@ app.get('/add', (req, res) => {
     res.render('add');
 });
 
-const port = 3000;
+app.post('/photos', async (req, res) => {
+    await Photo.create(req.body);
+    res.redirect('/');
+})
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
