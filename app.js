7b5bfd5cea31af8a4824c0f1703669bd58917d59
Eren ALTAY <eren60.altay@gmail.com>
2025-11-16 16:53:59 +0300

    Add photo editing and deletion functionality with method-override

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
+const methodOverride = require('method-override');
 // Load environment variables from .env into process.env
 require('dotenv').config();
 
@@ -20,6 +21,7 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(fileUpload());
+app.use(methodOverride('_method', { methods: ['POST', 'GET'] }));
 
 // Routes
 app.get('/', async (req, res) => {
@@ -65,6 +67,27 @@ app.post('/photos', async (req, res) => {
         });
         res.redirect('/');
     });
+});
+
+app.get('/photos/edit/:id', async (req, res) => {
+    const photo = await Photo.findById(req.params.id);
+    res.render('edit', { photo });
+});
+
+app.put('/photos/:id', async (req, res) => {
+    const photo = await Photo.findById(req.params.id);
+    photo.title = req.body.title;
+    photo.description = req.body.description;
+    photo.save();
+    res.redirect('/photos/' + req.params.id);
+});
+
+app.delete('/photos/:id', async (req, res) => {
+    const photo = await Photo.findById(req.params.id);
+    let deletedImage = __dirname + '/public' + photo.imageUrl;
+    fs.unlinkSync(deletedImage);
+    await Photo.findByIdAndDelete(req.params.id);
+    res.redirect('/');
 });
 
 const port = process.env.PORT || 3000;
